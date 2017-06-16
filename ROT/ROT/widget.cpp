5faa482019e78efe21d9378b3e43acc3010a5c11
Cedric Regenass <regenass.cedric@gmail.com>
2017-06-16 10:30:05 +0200

    added clear qDebug text. easier to follow program logic

--- ROT/ROT/widget.cpp
+++ ROT/ROT/widget.cpp
@@ -5,21 +5,21 @@
 
 Widget::Widget(QWidget *parent) : QWidget(parent)
 {
+    qDebug() <<"CREATE NEW ONE AND INSERT DATA...";
     rotTestClass* t1 = new rotTestClass(10);
-
     for (int i = 0; i < 20; i++ )
     {
         t1->push_back(i);
     }
     t1->COUT();
 
-    qDebug() <<"\nCREATE AND COPY NEW ONE...\n";
+    qDebug() <<"\nCREATE NEW ONE AND ASSIGN t1...";
     rotTestClass* t2 = new rotTestClass(12);
     t2 = t1;
     t2->COUT();
 
+    qDebug() <<"\nCREATE NEW ONE AND USE COPY CONSTRUCTOR TO COPY DATA FROM t2...";
     rotTestClass* t3 = new rotTestClass(*t2);
-
     t3->COUT();
 }
 
